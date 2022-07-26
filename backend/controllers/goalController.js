7079ac8c6fc46477c56fee2e4ff569ddf170149a
ahmedsmrt <ahmedsmrt@gmail.com>
2022-07-25 22:56:01 -0400

    Initial REST API for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -3,22 +3,34 @@
 // Also because we are using express there is a package that handles our async 
 const asyncHandler = require('express-async-handler')
 
+// We bring in our models in our Controller file
+const Goal = require("../models/goalModel")
+
 // @desc Get Goals
 // @route GET /api/goals
 // @access Private
 const getGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Get Goals" });
+    const goals = await Goal.find()
+
+    res.status(200).json(goals);
   })
 
 // @desc Set Goals
 // @route POST /api/goals
 // @access Private
 const setGoal = asyncHandler(async (req, res) => {
+
+    // Logic to check for text
     if(!req.body.text) {
         res.status(400)
         throw new Error("Please add a text field")
     }
-    res.status(200).json({ message: "Set Goals" });
+
+    const goal = await Goal.create( {
+        text:req.body.text
+    })
+
+    res.status(200).json(goal);
 
 })
 
@@ -26,14 +38,34 @@ const setGoal = asyncHandler(async (req, res) => {
 // @route PUT /api/goals/:id
 // @access Private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update Goals ${req.params.id}` });
+
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+        res.status(400)
+        throw new Error("Goal Not Find")
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true,})
+
+  res.status(200).json(updatedGoal);
 })
 
 // @desc Delete Goals
 // @route DELETE /api/goals/:id
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Delete Goals ${req.params.id}`})
+
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+        res.status(400)
+        throw new Error("Goal Not Find")
+    }
+
+    await goal.remove()
+
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
---
+++ backend/models/goalModel.js
@@ -0,0 +1,21 @@
+// Defining Schemea for particular resource
+
+const mongoose = require("mongoose");
+
+// The rules defining the way we consume data
+const goalSchema = mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: [true, "Please add a text value"],
+    },
+  },
+//   Creates an updated at and created at field
+  {
+    timestamps: true,
+  }
+);
+
+
+// Specifies that we are exporting not the whole const by the model that is named Goal and the schema that defines it
+module.exports = mongoose.model('Goal', goalSchema)
